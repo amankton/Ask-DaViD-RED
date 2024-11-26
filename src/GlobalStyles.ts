894aa1ce1d4e2582bcdfd390932fa5d91401c9d7
amankton <am.ankton@outlook.com>
2024-11-26 11:56:50 -0800

    "Add GlobalStyles file for consistent styling"

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${({ theme }) => theme.fonts.primary};
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.text};
+    line-height: 1.5;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    font-family: inherit;
+  }
+`;
+
+export default GlobalStyles;
